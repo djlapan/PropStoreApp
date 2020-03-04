7984c9b5d041c8016983012724957505b4ac2a24
Daniel LaPan <djlapan@yahoo.com>
2020-03-04 13:59:26 -0500

    Section 5 fix with part of Section 6

--- PropStore-UI/src/app/_services/error.interceptor.ts
+++ PropStore-UI/src/app/_services/error.interceptor.ts
@@ -24,7 +24,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                 if (serverError.errors && typeof serverError.errors === 'object') {
                     for (const key in serverError.errors) {
                         if (serverError.errors[key]) {
-                            modalStateErrors += serverError.error[key] + '\n';
+                            modalStateErrors += serverError.errors[key] + '\n';
                         }
                     }
                 }
